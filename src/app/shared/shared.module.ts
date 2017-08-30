7d351a2b3610ddc425f60c3b7f3aa584ced9f020
Tarek <trkmos@gmail.com>
2017-08-30 22:34:55 +0200

    adding navbar to shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { TopNavbarComponent } from './top-navbar.component';
+
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [TopNavbarComponent],
+  exports: [ TopNavbarComponent]
+})
+export class SharedModule { }
