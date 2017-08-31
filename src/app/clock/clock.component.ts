f0deefd8617f145d8dc517d9a5dca305b7641018
lordkhaledjamal <30383948+lordkhaledjamal@users.noreply.github.com>
2017-08-31 18:40:59 +0200

    d

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -1,91 +1,84 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipeComponent } from '../shared/date.pipe';
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 import { UsersService, xUser } from '../shared/users.service';
 var moment = require('moment');
 require('moment-precise-range-plugin');
 
-
-// import { SharedModule} from '../shared/shared.module';
 
 @Component({
   selector: 'app-clock',
   templateUrl: './clock.component.html',
   styles: []
 })
-export class ClockComponent  {
-private timeSheet:Array<xUser> = []
+
+
+export class ClockComponent {
+  private timeSheet: Array<xUser> = []
   today = '2017-08-31 01:00 PM'
- xDuration  
- ds
+  xDuration
+  ds
+  _shiftStart: boolean = true
+  lastUpdated
 
   
-    _shiftStart:boolean = true
-    lastUpdated
-  constructor(private usersService:UsersService) { 
+  constructor(private usersService: UsersService) {
     this.lastUpdated = new Date()
   }
 
-TogggleShift(){
-  this._shiftStart = !this._shiftStart
-  
-  
-  if (!this._shiftStart){
-
-    //start shift
+  TogggleShift() {
+    this._shiftStart = !this._shiftStart
+    if (!this._shiftStart) {
+      //start shift
       this.timeSheet.push(new xUser(4, new Date, "Open"))
       this.xDuration = Timer(this.today)
-      setInterval(()=> this.xDuration = Timer(this.today), 1000)
+      setInterval(() => this.xDuration = Timer(this.today), 1000)
       // @Team - this creates a new timeSheet instance. Throw it into the DB.
-  }else{
-    //end shift
+    } else {
+      //end shift
       updateTime(this.timeSheet, 4)
       // @Team - this function updates the timeSheet with the shift end date. Use it to update the service.   
+    }
+
   }
- 
-}
-ngOnInit(){
+  ngOnInit() {
     //pull timesheets to ds
     this.ds = this.usersService.getTimesheets()
-   
-   
+
+
     // rettrieve if user is online
-    if (isUserOnline(this.ds, "Khaled Jamal")){
+    if (isUserOnline(this.ds, "Khaled Jamal")) {
       this._shiftStart = !this._shiftStart
       this.xDuration = Timer(this.today)
-      setInterval(()=> this.xDuration = Timer(this.today), 1000)
+      setInterval(() => this.xDuration = Timer(this.today), 1000)
     }
-
+  }
 }
 
-getTime(){
- return moment.preciseDiff(this.today,moment().format("LLL"))}
+function Calc(y, x) {
+  return moment.utc(moment(x, "DD/MM/YYYY HH:mm:ss").diff(moment(y, "DD/MM/YYYY HH:mm"))).format("HH:mm")
 }
 
 
-
-function Calc(y, x){
-  return moment.utc(moment(x,"DD/MM/YYYY HH:mm:ss").diff(moment(y,"DD/MM/YYYY HH:mm"))).format("HH:mm")
-}
-
-function updateTime (array, x){
-    for(var i in array) {
-        if(array[i].id == x && array[i].endTime == "Open"){
-          array[i].endTime = new Date
-          break;
-        }
+function updateTime(array, x) {
+  for (var i in array) {
+    if (array[i].id == x && array[i].endTime == "Open") {
+      array[i].endTime = new Date
+      break;
     }
-}
-
-function Timer(today){
-  return moment.preciseDiff(today,moment().format("LLL"))
+  }
 }
 
 
-function isUserOnline(array, username):boolean{
-      for(var i in array) { 
-        if(array[i].username == username && array[i].timeout == "Open"){
-          return true
-        }
+function Timer(today) {
+  return moment.preciseDiff(today, moment().format("LLL"))
+}
+
+
+function isUserOnline(array, username): boolean {
+  for (var i in array) {
+    if (array[i].username == username && array[i].timeout == "Open") {
+      return true
     }
+  }
 }
