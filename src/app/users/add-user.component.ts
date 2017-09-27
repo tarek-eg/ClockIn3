f954acdf231c81877458a4d56d372920490ae56a
lordkhaledjamal <30383948+lordkhaledjamal@users.noreply.github.com>
2017-09-27 22:35:27 +0200

    Add User

--- src/app/users/add-user.component.ts
+++ src/app/users/add-user.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { UsersService, User } from '../shared/users.service';
+import { UsersService } from '../shared/users.service';
 import { EmitterService } from '../shared/emitter.service'
 import { FlashMessagesService } from 'angular2-flash-messages'
+import { User } from '../ServicesAPI/User'
 
 @Component({
   selector: 'app-add-user',
@@ -10,11 +11,15 @@ import { FlashMessagesService } from 'angular2-flash-messages'
 })
 export class AddUserComponent {
  
-  constructor(private flashMessage:FlashMessagesService,
-    private emitterService:EmitterService){}
   
-    addUser(){
-      this.emitterService.closeSidebar()
+  constructor(private flashMessage:FlashMessagesService,
+    // private emitterService:EmitterService
+  ){}
+  
+    
+    addUser(fname, lname, email){
+      // this.emitterService.closeSidebar()
+      console.log(new User(1, 1, fname +" " +lname, fname + lname, "123", 'red', "Egypt", 1, true, true ))
       this.flashMessage.show("User has been created.", {cssClass: 'alert-success',  timeout: 3000})
   
   }
