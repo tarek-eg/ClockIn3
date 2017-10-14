b96998dfc740642429d2a2632c05fe4144906d3e
lordkhaledjamal <30383948+lordkhaledjamal@users.noreply.github.com>
2017-10-14 09:31:29 +0200

    Fixing the single user load

--- src/app/users/user-profile.component.ts
+++ src/app/users/user-profile.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit , OnDestroy} from '@angular/core';
 import { Routes, Router, ActivatedRoute,ParamMap } from '@angular/router';
-import { UsersService, User } from '../shared/users.service';
+import { UsersService } from '../shared/users.service';
 import 'rxjs/add/operator/switchMap';
+import { UserService} from '../ServicesAPI/User.Service'
+import { User } from '../ServicesAPI/User'
 
 
 
@@ -14,12 +16,13 @@ export class UserProfileComponent implements OnInit, OnDestroy {
 
   userId: number;
   private sub: any;
-  user: User;
+  user: User[];
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private service: UsersService
+    private service: UsersService,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
@@ -28,10 +31,19 @@ export class UserProfileComponent implements OnInit, OnDestroy {
        // In a real app: dispatch action to load the details here.
     });
 
-this.route.paramMap
-    .switchMap((params: ParamMap) =>
-      this.service.getUser(params.get('id')))
-    .subscribe((user: User) => this.user = user);
+// this.route.paramMap
+//     .switchMap((params: ParamMap) =>
+//       this.service.getUser(params.get('id')))
+//     .subscribe((user: User) => this.user = user);
+
+    // Khaled J --> Start
+    // Khaled J --> commented out the paraMap
+    // Khaled J --> retrieve all users and filter a single user according the current user id
+    this.userService.getAllUsers().subscribe(user => this.user = user.filter(user => user.UserID === this.userId))
+    
+    // Khaled J --> Showing the result after 5 seconds as loading up 'this.user' take some time
+    setTimeout(() => console.log(this.user),5000)
+    // Khaled J --> End
   }
 
   ngOnDestroy() {
